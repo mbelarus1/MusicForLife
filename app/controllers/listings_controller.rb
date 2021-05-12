fd2f965a54ff5beeaa4511e702fb31365902e327
Bob Dijks <rhcdijks@gmail.com>
2021-05-11 21:55:31 -0500

    listings controller done

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,42 @@
 class ListingsController < ApplicationController
+  before_action :set_list
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @bookmark = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @listing = Listing.find(listing_params)
+  end
+
+  def update
+    @listing = Listing.find(listing_params)
+    @listing.update(params[:restaurant])
+    redirect_to listing_path(@listing)
+  end
+
+  def destroy
+    @listing = Listing.find(listing_params)
+    @listing.destroy
+    redirect_to listing_path
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:title, :description)
+  end
 end
