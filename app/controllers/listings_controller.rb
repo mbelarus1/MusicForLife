5706478e3b68c4d06a5616c3d8c5ba4b2305cfc6
Bob Dijks <rhcdijks@gmail.com>
2021-05-11 23:22:24 -0500

    fixed some details in the listing controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,46 @@
 class ListingsController < ApplicationController
+  before_action :set_list
+
+  def index
+    @listings = Listing.all
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = Listing.new(listing_params)
+    if @listing.save
+      redirect_to listing_path(@listing)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @listing = Listing.find(params[:id])
+  end
+
+  def update
+    @listing = Listing.find(params[:id])
+    @listing.update(listing_params])
+    redirect_to listing_path(@listing)
+  end
+
+  def destroy
+    @listing = Listing.find(params[:id])
+    @listing.destroy
+    redirect_to listing_path
+  end
+
+  private
+
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:title, :description)
+  end
 end
