e16d8ea898a2c571e8e9d93edee2956fb01b2ddd
Nikita Siniauski <nsiniauski@gmail.com>
2021-05-15 10:32:44 -0500

    trying to resolve issue, accidenta

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+    #Placeholder for random User selection
+    rand_user_id = rand(Model.count) # generates a number of Users
+    rand_user = User.first(:conditions => ["id >= ?", rand_user_id]
+    rand_booking_id = rand(Booking.count) # generates a number of Users
+    rand_booking = Booking.first(:conditions => ["id >= ?", rand_booking_id]
+    rand_listing_id = rand(Listing.count) # generates a number of Users
+    rand_listing = Listing.first(:conditions => ["id >= ?", rand_listing_id]
        #seeding Users
         20.times do
           usertest = User.create(
           username: Faker::Name.name,
           email: Faker::Internet.email,
-          password: "tertet",
+          password: "terteUset",
           )
          # puts "User creation..."
          # puts usertest.username
@@ -21,8 +28,34 @@
           listing = Listing.create(
           title: Faker::Music.instrument,
           description: Faker::Quote.matz,
+          listing.user = user
           )
-         puts "User creation..."
+         puts "Listing creation..."
+         puts listing.title
+          listing.save!
+        end
+#Seeding Reviews
+        20.times do
+          review = Review.create(
+          comment: "Oh my god, so Amazing",
+          rating: 5,
+          review.booking = rand_booking
+          )
+         puts "Review creation..."
+         puts listing.title
+          listing.save!
+        end
+#Seeding Booking
+        20.times do
+          booking = Booking.create(
+          start_date: "TBD",
+          end_date: "TBD",
+          price: 43,
+          booking.user = rand_user,
+          booking.listing = rand_listing,
+
+          )
+         puts "Review creation..."
          puts listing.title
           listing.save!
         end
