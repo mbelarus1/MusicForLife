d748d18fbea18501f8ac82bbe3047a205ae9e633
Nikita Siniauski <nsiniauski@gmail.com>
2021-05-15 13:19:21 -0500

    seed done(without pictures), minor correction to model files to fix relations

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,6 +1,6 @@
 class Booking < ApplicationRecord
-  belongs_to :listings
-  belongs_to :users
+  belongs_to :listing
+  belongs_to :user
   has_one :review, dependent: :destroy
   validates :start_date, presence: true
   validates :end_date, presence: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,61 +1,65 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
+
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
     #Placeholder for random User selection
-    rand_user_id = rand(Model.count) # generates a number of Users
-    rand_user = User.first(:conditions => ["id >= ?", rand_user_id]
-    rand_booking_id = rand(Booking.count) # generates a number of Users
-    rand_booking = Booking.first(:conditions => ["id >= ?", rand_booking_id]
-    rand_listing_id = rand(Listing.count) # generates a number of Users
-    rand_listing = Listing.first(:conditions => ["id >= ?", rand_listing_id]
+    # rand_user_id = rand(User.count) # generates a number of Users
+    # rand_user = User.first(:conditions => ["id >= ?", rand_user_id])
+    # rand_booking_id = rand(Booking.count) # generates a number of Users
+    # rand_booking = Booking.first(:conditions => ["id >= ?", rand_booking_id])
+    # rand_listing_id = rand(Listing.count) # generates a number of Users
+    # rand_listing = Listing.first(:conditions => ["id >= ?", rand_listing_id])
        #seeding Users
-        20.times do
+
+        5.times do
           usertest = User.create(
-          username: Faker::Name.name,
           email: Faker::Internet.email,
-          password: "terteUset",
-          )
+          password: "terteUset")
          # puts "User creation..."
-         # puts usertest.username
+         # puts usertest.usernamer
           usertest.save!
         end
+
 #Seeding listings
-        20.times do
+        10.times do
           listing = Listing.create(
           title: Faker::Music.instrument,
           description: Faker::Quote.matz,
-          listing.user = user
           )
+         listing.user = User.all.sample
          puts "Listing creation..."
          puts listing.title
           listing.save!
         end
-#Seeding Reviews
-        20.times do
-          review = Review.create(
-          comment: "Oh my god, so Amazing",
-          rating: 5,
-          review.booking = rand_booking
-          )
-         puts "Review creation..."
-         puts listing.title
-          listing.save!
-        end
-#Seeding Booking
-        20.times do
+
+    #Seeding Booking
+        5.times do
           booking = Booking.create(
           start_date: "TBD",
           end_date: "TBD",
-          price: 43,
-          booking.user = rand_user,
-          booking.listing = rand_listing,
+          price: 43
 
           )
-         puts "Review creation..."
-         puts listing.title
-          listing.save!
+          booking.user = User.all.sample
+          booking.listing =  Listing.all.sample
+         puts "Booking creation..."
+          booking.save!
         end
+#Seeding Reviews
+      Review.destroy_all
+        10.times do
+          review = Review.create(
+          comment: "Oh my god, so Amazing",
+          rating: 5,
+          )
+          review.booking = Booking.all.sample
+          review.user = User.all.sample
+         puts "Review creation..."
+
+          review.save!
+        end
+
+
