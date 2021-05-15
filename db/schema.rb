30fbef94f5458445fcbd7931525f8aac4f8d8e1f
mateopark <mateo900210@gmail.com>
2021-05-15 11:09:57 -0500

    html 5

--- db/schema.rb
+++ db/schema.rb
@@ -66,13 +66,9 @@ ActiveRecord::Schema.define(version: 2021_05_14_233604) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "username"
-    t.bigint "listing_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["listing_id"], name: "index_users_on_listing_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "users", "listings"
 end
