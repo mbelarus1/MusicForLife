9bb4e14169f241cbdb38f072d582b8fc4c943130
Nikita Siniauski <nsiniauski@gmail.com>
2021-05-18 20:00:37 -0500

    worked on seeding with image

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+require "open-uri"
 
 # Examples:
 #
@@ -30,10 +31,14 @@
           description: Faker::Quote.matz,
           )
          listing.user = User.all.sample
+        file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
          puts "Listing creation..."
          puts listing.title
           listing.save!
+          listing.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+        file.close
         end
+        # attaching image:
 
     #Seeding Booking
         5.times do
